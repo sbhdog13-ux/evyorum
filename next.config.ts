de87c6f9c0f30181a7ab25cc6fab28fc3e9524cb
Saltuk <saltukbugra@Mac.hgw.local>
2026-05-07 16:52:55 +0300

    Service Worker ve PWA ayarları eklendi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+// @ts-ignore
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
+module.exports = withPWA({
   /* config options here */
-};
-
-export default nextConfig;
+});
